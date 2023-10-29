a929fe125a27534df15353ea3a80485a4f68eb8e
viniAOliver <128255043+viniAOliver@users.noreply.github.com>
2023-10-29 17:07:50 -0300

    fix: adjusting to stable version of the flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:guys_on_work/app.dart';
 import 'package:guys_on_work/controllers/general_controller.dart';
 import 'package:guys_on_work/controllers/login_controller.dart';
-import 'package:device_preview/device_preview.dart';
 
 void main() {
   // calling the controllers
@@ -15,14 +13,5 @@ void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   // running the app
-  if (GetPlatform.isAndroid) {
-    runApp(const MainApp());
-  } else {
-    runApp(
-      DevicePreview(
-        builder: (context) => const MainApp(),
-        enabled: !kReleaseMode,
-      ),
-    );
-  }
+  runApp(const MainApp());
 }
